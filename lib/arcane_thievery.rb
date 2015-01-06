864c82d420d481ff4fc64f03a74e30e2e1b17bd8
Nathan Wise <nathanwise@gmail.com>
2015-01-06 13:57:08 -0500

    adding first end-point

--- lib/arcane_thievery.rb
+++ lib/arcane_thievery.rb
@@ -1,4 +1,5 @@
 require "arcane_thievery/version"
+require "arcane_thievery/guild_details"
 
 module ArcaneThievery
   # Your code goes here...
---
+++ lib/arcane_thievery/guild_details.rb
@@ -0,0 +1,9 @@
+module ArcaneThievery
+  class GuildDetails
+
+    def self.derp
+      puts 'derp'
+    end
+
+  end
+end
