884a94db719c31731280600a9c8cb0f83d336ab1
Nathan Wise <nathanwise@gmail.com>
2015-01-08 09:18:47 -0500

    Fixing syntax typo

--- lib/arcane_thievery/item_details.rb
+++ lib/arcane_thievery/item_details.rb
@@ -4,7 +4,7 @@ module ArcaneThievery
     URL = ArcaneThievery::Config.base_url + '/v1/item_details.json'
 
     def self.find(item_id)
-      request = Typhoeus::Request.new(self::URL, params: {item_id: item_id} method: :get)
+      request = Typhoeus::Request.new(self::URL, params: {item_id: item_id}, method: :get)
       response = request.run
 
       return JSON.parse(response.body)
